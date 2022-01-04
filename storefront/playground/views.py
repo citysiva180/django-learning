34b2a5df8b57ae6ba9360833879a2e417463426b
Siva Rajan <citysiva180@gmail.com>
2022-01-04 05:47:14 +0530

    Added Query SET ORM in views

--- storefront/playground/views.py
+++ storefront/playground/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from store.models import Product
 # Create your views here.
 
 # This http function is a default function and it accepts string as a message parameter
 
 
 def say_hello(request):
-    x = 1
-    y = 2
+    query_set = Product.objects.all()
+
+    for product in query_set:
+        print(product)
+    # pulling all objects,  get for getting all objectd and pull
+    # Data comming through ORM is a Query SET
+    # Evaluation happens on running a query set.
+    # It happens on Iteration,or conversion into a list or access an individual element
+
+    # QuerySets are Lazy. Remember, loading everything at a single shot will make things complicated.
     return render(request, 'hello.html', {"name": "Siva"})
 
 # post creating this function, we could then add this in the urls
