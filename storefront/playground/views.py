dea7c3da81635265ebd986dc01fec3db5ccc2c3f
Siva Rajan <citysiva180@gmail.com>
2022-01-05 06:19:10 +0530

    Completed - Filtering, Getting and Indexing sets

--- storefront/playground/views.py
+++ storefront/playground/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.exceptions import ObjectDoesNotExist
 from store.models import Product
 # Create your views here.
 
@@ -7,16 +8,22 @@ from store.models import Product
 
 
 def say_hello(request):
-    query_set = Product.objects.all()
-
-    for product in query_set:
-        print(product)
-    # pulling all objects,  get for getting all objectd and pull
-    # Data comming through ORM is a Query SET
-    # Evaluation happens on running a query set.
-    # It happens on Iteration,or conversion into a list or access an individual element
-
-    # QuerySets are Lazy. Remember, loading everything at a single shot will make things complicated.
-    return render(request, 'hello.html', {"name": "Siva"})
+    queryset = Product.objects.filter(last_update__year=2021)
+    return render(request, 'hello.html', {"name": "Siva", 'products': list(queryset)})
 
 # post creating this function, we could then add this in the urls
+
+# NOTES FOR RETIRVING OBJECTS
+
+# pulling all objects,  get for getting all objectd and pull
+# Data comming through ORM is a Query SET
+# Evaluation happens on running a query set.
+# It happens on Iteration,or conversion into a list or access an individual element
+# QuerySets are Lazy. Remember, loading everything at a single shot will make things complicated
+# queryset = Product.objects.all() Returns all the data in the table | This returns a Query Set
+# queryset = Product.objects.get(pk=1) Returns a single product of the specified ID | It Returns an Object
+# product = Product.objects.filter(pk=0).first()
+
+# NOTES FOR FILTERING OBJECTS
+
+# Comparison values
